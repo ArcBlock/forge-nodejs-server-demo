8bc50c2bad0a368f950dac31644cd7ada2b43dc8
polunzh <zhenqiang@arcblock.io>
2019-12-02 18:14:33 +0800

    init repo

---
+++ libs/env.js
@@ -0,0 +1,20 @@
+module.exports = {
+  chainId:
+    process.env.CHAIN_ID ||
+    process.env.chainId,
+  chainHost:
+    process.env.CHAIN_HOST ||
+    process.env.chainHost,
+  appId:
+    process.env.APP_ID ||
+    process.env.appId,
+  appName:
+    process.env.APP_NAME ||
+    process.env.appName,
+  appDescription:
+    process.env.APP_DESCRIPTION ||
+    process.env.appDescription,
+  baseUrl:
+    process.env.BASE_URL ||
+    process.env.baeUrl,
+};
